2b32a6b483b7c6b69de57343732631cc64af36b5
huaijinA <167151973+huaijinA@users.noreply.github.com>
2025-09-20 09:31:19 +0800

    Add files via upload
    first Maven spring boot

---
+++ Diet_comment/src/main/java/com/example/diet_comment/DietCommentApplication.java
@@ -0,0 +1,13 @@
+package com.example.diet_comment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietCommentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DietCommentApplication.class, args);
+    }
+
+}
