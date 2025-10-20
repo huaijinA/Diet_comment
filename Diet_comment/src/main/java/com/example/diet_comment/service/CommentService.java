730e0ef29b9dd972594dc40e01c9d844d86093d7
lijj3 <2184506442@qq.com>
2025-10-20 19:28:54 +0800

    0

---
+++ Diet_comment/src/main/java/com/example/diet_comment/service/CommentService.java
@@ -0,0 +1,8 @@
+package com.example.diet_comment.service;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.diet_comment.model.Comment;
+
+public interface CommentService {
+    Page<Comment> getCommentsByPostId(Page<Comment> page, Integer postId);
+}
