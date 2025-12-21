36fa244badb90f86d80aaf9db95369b077726dbf
k. <861090111@qq.com>
2025-12-21 16:25:06 +0800

    Change baseURL in request.js to '/api'

--- Diet_comment_front/src/utils/request.js
+++ Diet_comment_front/src/utils/request.js
@@ -3,8 +3,7 @@ import { ElMessage } from 'element-plus'
 
 // 创建axios实例
 const service = axios.create({
-  // baseURL: process.env.BASE_API,
-  baseURL: 'http://localhost:8080', // 从环境变量获取基础URL
+  baseURL: '/api', // 基础URL
   timeout: 30000,
 })
 
