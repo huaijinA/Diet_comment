5f108af218cbdbd367b38c762c8045a3a4d2df94
Will-6978 <3112158036@qq.com>
2025-12-26 15:46:55 +0800

    getdish

---
+++ Diet_comment/src/main/java/com/example/diet_comment/mapper/DishMapper.java
@@ -0,0 +1,7 @@
+package com.example.diet_comment.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.diet_comment.model.Dish;
+
+public interface DishMapper extends BaseMapper<Dish> {
+}
